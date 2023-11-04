8703430afc81584862c10611fb94718660340586
Alberto Arroyo <albertoarroyo@hotmail.es>
2023-11-04 17:29:34 +0100

    Al añadir contacto no se pueden meter letras en el telefono ni numeros en el nombre

--- Actividad2_V2_JFrame/src/vista/VistaAdd.java
+++ Actividad2_V2_JFrame/src/vista/VistaAdd.java
@@ -11,6 +11,8 @@ import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import controlador.ControladorActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class VistaAdd extends JFrame {
     private JLabel nombre;
@@ -29,7 +31,7 @@ public class VistaAdd extends JFrame {
         setTitle("Añadir Contacto");
         setBounds(100, 100, 400, 180);
         setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        setLayout(null); // No se recomienda, pero se mantiene por compatibilidad
+        getContentPane().setLayout(null); // No se recomienda, pero se mantiene por compatibilidad
         setResizable(false);
         setIconImage(Toolkit.getDefaultToolkit().getImage("img/icono16.png"));
 
@@ -44,28 +46,60 @@ public class VistaAdd extends JFrame {
         // Crear los componentes y configurarlos
         nombre = new JLabel("Nombre");
         nombre.setBounds(60, 10, 80, 20);
-        add(nombre);
+        getContentPane().add(nombre);
         
         campoNombre = new JTextField();
+        //Que solo se puedan introducir letras en el campo
+        campoNombre.addKeyListener(new KeyAdapter() {
+        	@Override
+        	public void keyTyped(KeyEvent e) {
+        		int key = e.getKeyChar();
+        		//donde empiezan y terminan los numeros en asci
+        		boolean mayusculas = key >=65 && key <=90;
+        		boolean minusculas = key >=97 && key <= 122;
+        		//Si se introduce algo que no sean letras mayusculas o minusculas
+        		if (!(mayusculas || minusculas)) {
+        			e.consume();
+        		}
+        		
+        	}
+        });
         campoNombre.setBounds(140, 10, 80, 20);
-        add(campoNombre);
+        getContentPane().add(campoNombre);
         
         telefono = new JLabel("Teléfono");
         telefono.setBounds(60, 40, 80, 20);
-        add(telefono);
+        getContentPane().add(telefono);
         
         
         campoTelefono = new JTextField();
+        //Condicion para no introducir letras Design->SeleccionarCampo->Key->KeyTyped
+        campoTelefono.addKeyListener(new KeyAdapter() {
+        	@Override
+        	public void keyTyped(KeyEvent e) {
+        		int key = e.getKeyChar();
+        		//donde empiezan y terminan los numeros en asci
+        		boolean numeros = key >=48 && key <=57;
+        		//Si se introduce algo que no sea numero no se introduce
+        		if (!numeros) {
+        			e.consume();
+        		}
+        		//No se pueden introducir mas de 9 numeros en el campo
+        		if(campoTelefono.getText().length()>=9) {
+        			e.consume();
+        		}
+        	}
+        });
         campoTelefono.setBounds(140, 40, 80, 20);
-        add(campoTelefono);
+        getContentPane().add(campoTelefono);
 
         botonOk = new JButton("Ok");
         botonOk.setBounds(100, 80, 80, 30);
-        add(botonOk);
+        getContentPane().add(botonOk);
 
         botonCancel = new JButton("Cancel");
         botonCancel.setBounds(200, 80, 80, 30);
-        add(botonCancel);
+        getContentPane().add(botonCancel);
     }
 
 
