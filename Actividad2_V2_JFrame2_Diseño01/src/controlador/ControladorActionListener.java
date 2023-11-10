540ce66be2b7b28637d4519c9ffeb446646df6be
Alberto Arroyo <albertoarroyo@hotmail.es>
2023-11-10 15:35:45 +0100

    Pruebas diseño 01

---
+++ Actividad2_V2_JFrame2_Diseño01/src/controlador/ControladorActionListener.java
@@ -0,0 +1,182 @@
+package controlador;
+
+import java.awt.event.ActionEvent;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+import vista.VistaPrincipal;
+import vista.VistaSecundaria;
+
+
+public class ControladorActionListener implements ActionListener{
+	
+	//referencia a la vista creando una variable
+	 private VistaPrincipal vistaPrincipal;
+	 private VistaSecundaria vistaSecundaria;
+
+	 
+	//Inicializar la variable en el constructor
+	 
+	public ControladorActionListener(VistaPrincipal vista) {
+		this.vistaPrincipal = vista;
+		
+	}
+
+	@Override
+    public void actionPerformed(ActionEvent e) {
+		//Aqui ponemos todo lo que hace click cuando le damos a los botones	
+        
+		//Al pulsar boton añadir de la principal, abre secundaria y llama a los liseners de VistaAdd
+		if (e.getSource() == vistaPrincipal.getBotonAdd()) {
+			if(vistaSecundaria==null) {
+				vistaSecundaria = new VistaSecundaria(this);
+				vistaSecundaria.setTitle("Añadir contacto");
+				
+			}else {
+				resetCampos();
+				vistaSecundaria.setTitle("Añadir contacto");
+				vistaSecundaria.setVisible(true);
+			}
+			
+			//vistaAdd.setVisible(true);
+
+           // vistaAdd.getCampoNombre().requestFocus();
+        }
+		
+		//Al pulsar boton OK de añadir contacto, añade nombre y telefono a la tabla
+		if (e.getSource() == vistaSecundaria.getBotonOk()) {
+			
+			if("Añadir contacto".equals(vistaSecundaria.getTitle())) {
+				addDatosTabla();
+				resetCampos();
+		          //  vistaAdd.setVisible(false);
+		        vistaSecundaria.dispose();
+			}if("Editar contacto".equals(vistaSecundaria.getTitle())) {
+				editDatosTabla();
+				resetCampos();
+				vistaSecundaria.dispose();
+			}         
+        }
+		
+		//Al pulsar cancelar de añadir contacto cierra y queda la principal
+		//else if (vistaAdd != null && e.getSource() == vistaAdd.getBotonCancel()) {
+		if (e.getSource() == vistaSecundaria.getBotonCancel()) {
+            vistaSecundaria.dispose();
+        }
+		
+		//Al pulsar el boton editar de la tabla principal
+		if (e.getSource() == vistaPrincipal.getBotonEdit()) {
+			
+            //vistaEditar.getCampoNombre().requestFocus();
+            int selectedRow = vistaPrincipal.obtenerFilaSeleccionada();
+            
+    		if (selectedRow >= 0) {
+    			if(vistaSecundaria==null) {
+    				vistaSecundaria = new VistaSecundaria(this);
+    			}else {
+    				vistaSecundaria.setVisible(true);
+    			}
+    			vistaSecundaria.setTitle("Editar contacto");
+    			ponerDatosTabla();
+    				
+    		}else {
+    			JOptionPane.showMessageDialog(null,"Tiene que seleccionar una fila", "Aviso", JOptionPane.INFORMATION_MESSAGE);	
+    		}
+                
+        }
+		//Al pulsar el boton borrar de la tabla principal
+		if (e.getSource() == vistaPrincipal.getBotonDelete()) {
+		        eliminarContacto();
+		}
+		
+    }
+			
+	//metodo para añadir los datos de los campos nombre y telefono a la tabla
+	public void addDatosTabla() {
+	    String nombre = vistaSecundaria.getCampoNombre().getText();
+	    String telefono = vistaSecundaria.getCampoTelefono().getText();
+	    if(nombre.isEmpty()){
+	        JOptionPane.showMessageDialog(null,"Nombre está vacío", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+	        if (vistaSecundaria.isVisible()) {
+	            vistaSecundaria.dispose(); // Cierra la ventana si está visible
+	        }
+	        vistaSecundaria = null; // Reinicializa la instancia de VistaAdd
+	    } else if(telefono.isEmpty()){
+	        JOptionPane.showMessageDialog(null,"Teléfono está vacío", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+	        if (vistaSecundaria.isVisible()) {
+	            vistaSecundaria.dispose(); // Cierra la ventana si está visible
+	        }
+	        vistaSecundaria = null; // Reinicializa la instancia de VistaAdd
+	    } else {
+	        DefaultTableModel tableModel = vistaPrincipal.getTableModel();
+	        tableModel.addRow(new String[]{nombre, telefono});
+	       // resetCampos();
+	    }
+	}
+	
+	public void ponerDatosTabla() {
+		int selectedRow = vistaPrincipal.obtenerFilaSeleccionada();
+
+		if (selectedRow >= 0) {
+		    // Seleccionar la fila se encuentra en 'selectedRow'
+		    // Puedes acceder a los datos de esa fila a través del modelo de tabla
+		    String nombre = vistaPrincipal.getTableModel().getValueAt(selectedRow, 0).toString();
+		    String telefono = vistaPrincipal.getTableModel().getValueAt(selectedRow, 1).toString();
+
+	        // Asignar los valores a los campos de edición en vistaEditar
+		    
+	        vistaSecundaria.getCampoNombre().setText(nombre);
+	        vistaSecundaria.getCampoTelefono().setText(telefono);
+		}
+		
+	}
+	public void editDatosTabla() {
+		
+		int filaSeleccionada = vistaPrincipal.obtenerFilaSeleccionada();
+
+		if (filaSeleccionada >= 0) {
+		   
+	       // Asignar los valores a los campos de edición en vistaEditar
+	        //Añadir los valores editados
+	        String nombreEditado = vistaSecundaria.getCampoNombre().getText();
+	    	String telefonoEditado = vistaSecundaria.getCampoTelefono().getText();
+	    	if(nombreEditado.isEmpty()){
+	    		JOptionPane.showMessageDialog(null,"Nombre esta vacio", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+	    	}else if(telefonoEditado.isEmpty()){
+	    		JOptionPane.showMessageDialog(null,"Telefono esta vacio", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+	    	}else {
+	    		DefaultTableModel tableModel = vistaPrincipal.getTableModel();
+	    		tableModel = vistaPrincipal.getTableModel();
+	    		tableModel.setValueAt(nombreEditado, filaSeleccionada, 0);
+	    	    tableModel.setValueAt(telefonoEditado, filaSeleccionada, 1);
+	    		
+	    	}  
+	        
+        } else {
+            // Mostrar un mensaje al usuario indicando que no hay fila seleccionada.
+            JOptionPane.showMessageDialog(vistaPrincipal, "Selecciona un contacto para editar.", "Sin selección", JOptionPane.INFORMATION_MESSAGE);
+        }
+
+	}
+	public void eliminarContacto() {
+		int filaSeleccionada = vistaPrincipal.obtenerFilaSeleccionada();
+
+		if (filaSeleccionada >= 0) {
+			DefaultTableModel tableModel = vistaPrincipal.getTableModel();
+			tableModel.removeRow(filaSeleccionada);
+		}
+		else {
+            // Mostrar un mensaje al usuario indicando que no hay fila seleccionada.
+            JOptionPane.showMessageDialog(vistaPrincipal, "Selecciona un contacto para editar.", "Sin selección", JOptionPane.INFORMATION_MESSAGE);
+        }
+		
+	}
+	public void resetCampos() {
+		vistaSecundaria.getCampoNombre().setText(null);
+        vistaSecundaria.getCampoTelefono().setText(null);
+	}
+	 	 
+}
---
+++ Actividad2_V2_JFrame2_Diseño01/src/vista/VistaSecundaria.java
@@ -0,0 +1,126 @@
+package vista;
+
+import java.awt.Toolkit;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import javax.swing.JTextField;
+
+
+import controlador.ControladorActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class VistaSecundaria extends JDialog {
+    private JLabel nombre;
+    private JLabel telefono;
+    private JButton botonOk;
+    private JButton botonCancel;
+    private JTextField campoNombre;
+    private JTextField campoTelefono;
+    private ControladorActionListener controlador;
+    
+    //le pasamos por parametro el controlador
+    public VistaSecundaria(ControladorActionListener controlador) {
+        this.controlador = controlador;
+
+        // Crear la ventana y establecerla
+        setTitle("Añadir Contacto");
+        setBounds(100, 100, 400, 180);
+        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        getContentPane().setLayout(null); // No se recomienda, pero se mantiene por compatibilidad
+        setResizable(false);
+        setIconImage(Toolkit.getDefaultToolkit().getImage("img/icono16.png"));
+
+        // Llamar a initVariables para inicializar los componentes
+        initVariables();
+
+        // Hacer visible la ventana (debe ser la última opción del constructor)
+        setVisible(true);
+    }
+
+    private void initVariables() {
+        // Crear los componentes y configurarlos
+        nombre = new JLabel("Nombre");
+        nombre.setBounds(60, 10, 80, 20);
+        getContentPane().add(nombre);
+        
+        campoNombre = new JTextField();
+        //Que solo se puedan introducir letras en el campo
+        campoNombre.addKeyListener(new KeyAdapter() {
+        	@Override
+        	public void keyTyped(KeyEvent e) {
+        		int key = e.getKeyChar();
+        		//donde empiezan y terminan los numeros en asci
+        		boolean mayusculas = key >=65 && key <=90;
+        		boolean minusculas = key >=97 && key <= 122;
+        		boolean espacio = key == 32;
+        		//Si se introduce algo que no sean letras mayusculas o minusculas
+        		if (!(mayusculas || minusculas || espacio)) {
+        			e.consume();
+        		}
+        		
+        	}
+        });
+        campoNombre.setBounds(140, 10, 80, 20);
+        getContentPane().add(campoNombre);
+        
+        telefono = new JLabel("Teléfono");
+        telefono.setBounds(60, 40, 80, 20);
+        getContentPane().add(telefono);
+        
+        
+        campoTelefono = new JTextField();
+        //Condicion para no introducir letras Design->SeleccionarCampo->Key->KeyTyped
+        campoTelefono.addKeyListener(new KeyAdapter() {
+        	@Override
+        	public void keyTyped(KeyEvent e) {
+        		int key = e.getKeyChar();
+        		//donde empiezan y terminan los numeros en asci
+        		boolean numeros = key >=48 && key <=57;
+        		//Si se introduce algo que no sea numero no se introduce
+        		if (!numeros) {
+        			e.consume();
+        		}
+        		//No se pueden introducir mas de 9 numeros en el campo
+        		if(campoTelefono.getText().length()>=9) {
+        			e.consume();
+        		}
+        	}
+        });
+        campoTelefono.setBounds(140, 40, 80, 20);
+        getContentPane().add(campoTelefono);
+
+        botonOk = new JButton("Ok");
+        botonOk.addActionListener(controlador);
+        botonOk.setBounds(100, 80, 80, 30);
+        getContentPane().add(botonOk);
+
+        botonCancel = new JButton("Cancel");
+        botonCancel.addActionListener(controlador);
+        botonCancel.setBounds(200, 80, 80, 30);
+        getContentPane().add(botonCancel);
+    }
+
+    //getters
+
+	public JTextField getCampoNombre() {
+		return campoNombre;
+	}
+
+	public JTextField getCampoTelefono() {
+		return campoTelefono;
+	}
+    public JButton getBotonOk() {
+		return botonOk;
+	}
+
+	public JButton getBotonCancel() {
+		return botonCancel;
+	}
+
+}
+
