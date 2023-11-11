ac125eef990d2982b29b0efdd7af17c7a0caf91c
Alberto Arroyo <albertoarroyo@hotmail.es>
2023-11-11 11:24:14 +0100

    Diseño 1.0 negro

--- Actividad2_V2_JFrame2_Diseño01/src/vista/VistaPrincipal.java
+++ Actividad2_V2_JFrame2_Diseño01/src/vista/VistaPrincipal.java
@@ -13,14 +13,17 @@ import javax.swing.JLabel;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-
+import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableModel;
 
 import controlador.ControladorActionListener;
 import javax.swing.JPanel;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.Image;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 import java.awt.SystemColor;
 
@@ -29,7 +32,7 @@ public class VistaPrincipal extends JFrame{
 	//Atributos de la clase, componentes que vamod a necesitar
 	
 	//Icono
-	private JLabel icono;
+	private JLabel icono, nombreApp;
 	//Botones 
 	private JButton botonAdd;
 	private JButton botonEdit;
@@ -45,11 +48,12 @@ public class VistaPrincipal extends JFrame{
 	private JScrollPane scrollPane;
 	private JPanel panel;
 	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
 	
 	//Constructor para inicializar las variables
 	public VistaPrincipal() {
 		//crear la ventana y establecerla
-		setBounds(100,100,530,359);
+		setBounds(100,100,515,390);
 		//establecer lo que hace el boton cerrar X
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//decimos que no vamos a utilizar layout, lo posicionamos nosotros
@@ -78,8 +82,8 @@ public class VistaPrincipal extends JFrame{
 		tableModel = new DefaultTableModel(nombreColumnas,0);
 		
 		JPanel background = new JPanel();
-		background.setBackground(Color.WHITE);
-		background.setBounds(0, 0, 530, 322);
+		background.setBackground(Color.LIGHT_GRAY);
+		background.setBounds(0, 0, 506, 353);
 		getContentPane().add(background);
 		background.setLayout(null);
 		
@@ -89,7 +93,7 @@ public class VistaPrincipal extends JFrame{
 		//3º meter la tabla en el Scrollpane
 		
 		scrollPane = new JScrollPane(tablaContactos);
-		scrollPane.setBounds(201, 11, 300, 300);
+		scrollPane.setBounds(196, 46, 300, 298);
 		background.add(scrollPane);
 		
 		
@@ -112,16 +116,58 @@ public class VistaPrincipal extends JFrame{
 		background.add(botonEdit);
 			
 		panel = new JPanel();
-		panel.setBackground(SystemColor.textHighlight);
-		panel.setBounds(0, 0, 191, 322);
+		panel.setBackground(new Color(0, 0, 0));
+		panel.setBounds(0, 0, 191, 353);
 		background.add(panel);
 		
-		//crear el ICONO
-		Image img = new ImageIcon("icono256.png").getImage();
-		icono = new JLabel(new ImageIcon(img.getScaledInstance(80,80, Image.SCALE_SMOOTH)));
-		icono.setBounds(90, 20, 80, 80);
-		getContentPane().add(icono);
+
 		
+		/*
+		//crear el componente
+		icono = new JLabel();
+		icono.setIcon(new ImageIcon("img/contactos.png"));
+		//colocarlo en la ventana
+		icono.setBounds(20, 20, 128, 128);
+		//añadir el icono al panel
+		panel.add(icono);
+	
+	*/
+		//crear el ICONO **********USAR ESTE QUE REDIMENSIONA
+		
+		Image img = new ImageIcon("img/contactos.png").getImage();
+		icono = new JLabel(new ImageIcon(img.getScaledInstance(80, 80, Image.SCALE_SMOOTH)));
+		icono.setBounds(50, 50, 80, 80);
+		panel.add(icono);
+		
+		
+		//Cambiar la fuente de la ventana
+		
+		try {
+			Font font = Font.createFont(Font.TRUETYPE_FONT, getClass().getResourceAsStream("TERMINAT.ttf"));
+			
+			JPanel panel_1 = new JPanel();
+			panel_1.setBackground(new Color(0, 0, 0));
+			panel_1.setBounds(191, 0, 325, 43);
+			background.add(panel_1);
+			//crear la label con esa fuente
+			nombreApp = new JLabel("MIS CONTACTOS");
+			panel_1.add(nombreApp);
+			//alineacion centrada
+			nombreApp.setHorizontalAlignment(SwingConstants.CENTER);
+			//meter la fuente
+			nombreApp.setFont(font.deriveFont(20f));
+			//colocar la etiqueta
+			nombreApp.setBounds(0, 10, 20, 10);
+			//color de la letra en blanco
+			nombreApp.setForeground(Color.WHITE);
+			
+		} catch (FontFormatException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
 		
 	}
